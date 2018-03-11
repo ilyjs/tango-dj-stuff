9003dc7d71887e0115c0d5e27b4e411d5ffbcdea
kirjs <kirill@kcherkashin.org>
2018-03-11 13:49:53 -0400

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,45 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <Grid :songs="songs"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import csvtojson from 'csvtojson';
+    import Grid from './components/Grid.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'app',
+
+        created() {
+            const songs = [];
+            fetch('https://docs.google.com/spreadsheets/d/e/2PACX-1vSndXZzM-gWDDNpt8HqSTPcT32OgHLyJovPYYV98AA-tRO4zlrbk4sV1EqRBYmz0UgMWswtN7M2Bk-j/pub?output=csv').then(a => a.text()).then(a => {
+                csvtojson({noheader: true})
+                    .fromString(a)
+                    .on('json', (song) => {
+                        songs.push(song);
+                    })
+                    .on('done', () => {
+                        this.songs = songs;
+                    });
+            })
+
+
+        },
+        data() {
+            return {songs: []}
+        },
+        components: {
+            Grid
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -4,7 +4,7 @@
 
 <script>
     export default {
-        name: 'HelloWorld',
+        name: 'Grid',
         props: {
             msg: String
         }
