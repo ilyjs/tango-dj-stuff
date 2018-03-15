cf31889372ad134881fa75932310c3fdcad57260
voicetime <iprovod@gmail.com>
2018-03-15 03:42:01 +0300

    add auth spotify and install query-string

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -8,7 +8,7 @@
 </template>
 
 <script>
-
+import queryString from 'query-string'
     const titles = [{
         prop: "name",
         label: "Name"
@@ -24,9 +24,15 @@
     }, {
         prop: "year",
         label: "year"
-    }
+    },
+
     ];
 
+    const logIn = ()=>{
+      const parsedHash = queryString.parse(location.hash);
+      if(parsedHash.access_token) return parsedHash.access_token
+      return false
+    }
 
     export default {
         name: 'Grid',
@@ -46,12 +52,22 @@
                             console.log(row.name);
                             console.log(row.vocal);
 
-                            // TODO:
+                          //window.location.replace('https://accounts.spotify.com/authorize?client_id=21c0c52d617f4cb09df75223b6d484a0&redirect_uri=http:%2F%2Flocalhost:8080/&response_type=token')
+
+                          const FETCH_URL =  "https://api.spotify.com/v1/search?q=MILONGON+Francisco&type=track"
+                          fetch(FETCH_URL, {
+                            method: 'GET',
+                            headers: {
+                              'Authorization': "Bearer BQAyxYYQMmoCMcvHF_KYUO30XLJjQkdqKKrReLZAhJz4rvFGlLcbXoZ6Dlj8BositlQ9XaSf7C7wVlRX2Lm-cdxRTo-8hX7M2XqXhL9i8gwFa98z1qz1Aj9BL4XggpBou0evlp1uEDeq9DqUnfoO7iru8Sh6Re8"
+                            }
+                          }).then(response => response.json())
+                            .then(json => console.log('json', json));
+
+                          // TODO:
                             // 1. Query Spotify
                             // 2. Let the user pick ths song
                             // 3. Get the song ID and song BPM (from metadata)
                             // 4. Connect to google sheets and upload spotifyID and bpm data to appropriate location
-                            debugger;
                         },
                         buttonProps: {
                             type: 'text'
